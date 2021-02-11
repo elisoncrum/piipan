68ca8e0a61d54ebbefc567f46c5191751d8d750b
Matt Henry <matt.henry@gsa.gov>
2021-02-11 14:03:44 -0500

    Add basic server-side form validation

--- query-tool/src/Piipan.QueryTool/PiiRecord.cs
+++ query-tool/src/Piipan.QueryTool/PiiRecord.cs
@@ -5,22 +5,31 @@ namespace Piipan.QueryTool
 {
     public class PiiRecord
     {
+        [Required]
         [Display(Name = "First Name")]
         [JsonPropertyName("first")]
         public string FirstName { get; set; }
 
+        [Required]
         [Display(Name = "Middle Name")]
         [JsonPropertyName("middle")]
         public string MiddleName { get; set; }
 
+        [Required]
         [Display(Name = "Last Name")]
         [JsonPropertyName("last")]
         public string LastName { get; set; }
 
+        [Required]
+        [RegularExpression(@"^\d{4}-\d{2}-\d{2}$",
+            ErrorMessage = "Birth date must have the form YYYY-MM-DD")]
         [Display(Name = "Date of Birth")]
         [JsonPropertyName("dob")]
         public string DateOfBirth { get; set; }
 
+        [Required]
+        [RegularExpression(@"^\d{3}-\d{2}-\d{4}$",
+            ErrorMessage = "SSN must have the form 000-00-0000")]
         [Display(Name = "SSN")]
         [JsonPropertyName("ssn")]
         public string SocialSecurityNum { get; set; }
