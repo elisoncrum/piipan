a1b848e64c745193d7f666e08e72bbde74d36726
Eleni Chappen <eleni.chappen@gsa.gov>
2021-02-12 17:05:37 -0600

    Most basic implementation of showing participant uploads - need to be on GSA network for api request to work - need to allow your host in CORS config for function app - The request is done through vanilla js fetch

---
+++ dashboard/src/Piipan.Dashboard/Pages/ParticipantUploads.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Piipan.Dashboard.Pages
+{
+    public class ParticipantUploadsModel : PageModel
+    {
+
+        public string Title = "Participant Uploads";
+
+        public void OnGet()
+        {}
+    }
+}
