77b37bd26ac124ed75959d547cb2a43abed3d21a
Michael King <michael.r.king@gsa.gov>
2021-04-21 15:54:01 -0400

    Validate lookup ID input

--- query-tool/src/Piipan.QueryTool/Lookup.cs
+++ query-tool/src/Piipan.QueryTool/Lookup.cs
@@ -1,4 +1,3 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
@@ -7,6 +6,8 @@ namespace Piipan.QueryTool
     public class Lookup : IQueryable
     {
         [Required]
+        [RegularExpression(@"^[BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz2-9]{7}$",
+            ErrorMessage = "Lookup ID must be 7 characters. It can not contain vowels or the numbers 0 or 1.")]
         [Display(Name = "Lookup ID")]
         [JsonPropertyName("lookup_id")]
         public string LookupId { get; set; }
