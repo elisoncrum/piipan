09db9d4da39b2632379031d2aee00c6c6a24d143
Eleni Chappen <eleni.chappen@gsa.gov>
2021-06-02 12:02:57 -0500

    revert nullable enabling on QueryTool PiiRecord

--- query-tool/src/Piipan.QueryTool/PiiRecord.cs
+++ query-tool/src/Piipan.QueryTool/PiiRecord.cs
@@ -2,14 +2,12 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
-#nullable enable
-
 namespace Piipan.QueryTool
 {
     public class MatchRequest
     {
         [JsonPropertyName("query")]
-        public PiiRecord? Query { get; set; }
+        public PiiRecord Query { get; set; }
     }
 
     public class PiiRecord : IQueryable
@@ -17,49 +15,49 @@ namespace Piipan.QueryTool
         [Required]
         [Display(Name = "First name")]
         [JsonPropertyName("first")]
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; }
 
         [Display(Name = "Middle name")]
         [JsonPropertyName("middle")]
-        public string? MiddleName { get; set; }
+        public string MiddleName { get; set; }
 
         [Required]
         [Display(Name = "Last name")]
         [JsonPropertyName("last")]
-        public string? LastName { get; set; }
+        public string LastName { get; set; }
 
         [Required]
         [Display(Name = "Date of birth")]
         [DataType(DataType.Date),
             DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         [JsonPropertyName("dob")]
-        public DateTime? DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
 
         [Required]
         [RegularExpression(@"^\d{3}-\d{2}-\d{4}$",
             ErrorMessage = "SSN must have the form 000-00-0000")]
         [Display(Name = "SSN")]
         [JsonPropertyName("ssn")]
-        public string? SocialSecurityNum { get; set; }
+        public string SocialSecurityNum { get; set; }
 
         [Display(Name = "State")]
         [JsonPropertyName("state")]
-        public string? State { get; set; }
+        public string State { get; set; }
 
         [Display(Name = "CaseId")]
         [JsonPropertyName("case_id")]
-        public string? CaseId { get; set; }
+        public string CaseId { get; set; }
 
         [Display(Name = "ParticipantId")]
         [JsonPropertyName("participant_id")]
-        public string? ParticipantId { get; set; }
+        public string ParticipantId { get; set; }
 
         [Display(Name = "Lookup ID")]
         [JsonPropertyName("lookup_id")]
-        public string? LookupId { get; set; }
+        public string LookupId { get; set; }
 
         [Display(Name = "Benefits End Month")]
         [JsonPropertyName("benefits_end_month")]
-        public string? BenefitsEndMonth { get; set; }
+        public string BenefitsEndMonth { get; set; }
     }
 }
