a32755a8a1785d08167b0b172f1c0c0eee28ed79
Tim Ballard <timothy.ballard@gsa.gov>
2021-07-26 14:47:15 -0500

    log an error if email claim not found, update tests

--- shared/src/Piipan.Shared/ClaimsProvider.cs
+++ shared/src/Piipan.Shared/ClaimsProvider.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Security.Claims;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Piipan.Shared.Claims
@@ -7,18 +8,33 @@ namespace Piipan.Shared.Claims
     public class ClaimsProvider : IClaimsProvider
     {
         private readonly ClaimsOptions _options;
+        private readonly ILogger<ClaimsProvider> _logger;
 
-        public ClaimsProvider(IOptions<ClaimsOptions> options)
+        public ClaimsProvider(IOptions<ClaimsOptions> options,
+            ILogger<ClaimsProvider> logger)
         {
             _options = options.Value;
+            _logger = logger;
         }
 
         public string GetEmail(ClaimsPrincipal claimsPrincipal)
         {
-            return claimsPrincipal
-                .Claims
-                .Single(c => c.Type == _options.Email)
-                .Value;
+            try
+            {
+                return claimsPrincipal
+                    .Claims
+                    .Single(c => c.Type == _options.Email)
+                    .Value;
+            }
+            catch (System.InvalidOperationException)
+            {
+                _logger.LogError($"Unable to extract claim with type {_options.Email} for current user!");
+                foreach (var claim in claimsPrincipal.Claims)
+                {
+                    _logger.LogDebug($"[CLAIM] {claim.Type}: {claim.Value}");
+                }
+                throw;
+            }
         }
     }
 }
--- shared/tests/Piipan.Shared.Tests/ClaimsProviderTests.cs
+++ shared/tests/Piipan.Shared.Tests/ClaimsProviderTests.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Security.Claims;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Moq;
 using Xunit;
 
 namespace Piipan.Shared.Claims.Tests
@@ -11,10 +14,12 @@ namespace Piipan.Shared.Claims.Tests
         public void GetEmail()
         {
             // Arrange
+            var logger = new Mock<ILogger<ClaimsProvider>>();
+
             var options = Options.Create<ClaimsOptions>(new ClaimsOptions {
                 Email = "email_claim_type"
             });
-            var claimsProvider = new ClaimsProvider(options);
+            var claimsProvider = new ClaimsProvider(options, logger.Object);
             var claimsPrincipal = new ClaimsPrincipal();
             claimsPrincipal.AddIdentity(new ClaimsIdentity(new List<Claim> {
                 new Claim("email_claim_type", "noreply@tts.test")
@@ -31,10 +36,12 @@ namespace Piipan.Shared.Claims.Tests
         public void GetEmail_NotFound()
         {
             // Arrange
+            var logger = new Mock<ILogger<ClaimsProvider>>();
+
             var options = Options.Create<ClaimsOptions>(new ClaimsOptions {
                 Email = "email_claim_type"
             });
-            var claimsProvider = new ClaimsProvider(options);
+            var claimsProvider = new ClaimsProvider(options, logger.Object);
             var claimsPrincipal = new ClaimsPrincipal();
             claimsPrincipal.AddIdentity(new ClaimsIdentity(new List<Claim> {
                 new Claim("email_claim_type_different", "noreply@tts.test")
@@ -42,6 +49,13 @@ namespace Piipan.Shared.Claims.Tests
 
             // Act / Assert
             Assert.Throws<System.InvalidOperationException>(() => claimsProvider.GetEmail(claimsPrincipal));
+
+            logger.Verify(m => m.Log(
+                It.Is<LogLevel>(l => l == LogLevel.Error),
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((object v, Type _) => v.ToString().Contains("Unable to extract claim with type email_claim_type for current user!")),
+                It.IsAny<Exception>(),
+                (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()), Times.Once());
         }  
     }
 }
