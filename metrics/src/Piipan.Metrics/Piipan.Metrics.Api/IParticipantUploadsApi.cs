19693f5fe31bff95b6c92e8d9f95146d8052556f
Tim Ballard <timothy.ballard@gsa.gov>
2021-09-13 16:05:12 -0500

    add core tests coverage

--- metrics/src/Piipan.Metrics/Piipan.Metrics.Api/IParticipantUploadsApi.cs
+++ metrics/src/Piipan.Metrics/Piipan.Metrics.Api/IParticipantUploadsApi.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 
+#nullable enable
+
 namespace Piipan.Metrics.Api
 {
     public interface IParticipantUploadApi
--- metrics/src/Piipan.Metrics/Piipan.Metrics.Api/Meta.cs
+++ metrics/src/Piipan.Metrics/Piipan.Metrics.Api/Meta.cs
@@ -1,5 +1,7 @@
 using System;
 
+#nullable enable
+
 namespace Piipan.Metrics.Api
 {
     public class Meta
