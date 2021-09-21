2d56d3ae135e376428a9d27c6c7a2b097f44e035
Tim Ballard <timothy.ballard@gsa.gov>
2021-09-21 12:20:22 -0500

    add participants service registration extension

---
+++ participants/src/Piipan.Participants/Piipan.Participants.Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Piipan.Participants.Api;
+using Piipan.Participants.Core.DataAccessObjects;
+using Piipan.Participants.Core.Services;
+
+namespace Piipan.Participants.Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void RegisterParticipantsServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IParticipantDao, ParticipantDao>();
+            serviceCollection.AddTransient<IUploadDao, UploadDao>();
+            serviceCollection.AddTransient<IParticipantApi, ParticipantService>();
+        }
+    }
+}
