855bf6509cb5f3c1c161b6e09911371dfa5dea9b
Tim Ballard <timothy.ballard@gsa.gov>
2021-09-21 12:25:01 -0500

    add tests for participants service registration extension

---
+++ participants/tests/Piipan.Participants.Core.Tests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,31 @@
+using System.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Piipan.Participants.Core.Extensions;
+using Moq;
+using Xunit;
+using Piipan.Participants.Core.DataAccessObjects;
+using Piipan.Participants.Api;
+
+namespace Piipan.Participants.Core.Tests.Extensions
+{
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void RegisterCoreServices_AllResolve()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            //services.AddLogging();
+            services.AddTransient<IDbConnection>(c => Mock.Of<IDbConnection>());
+
+            // Act
+            services.RegisterParticipantsServices();
+            var provider = services.BuildServiceProvider();
+
+            // Assert
+            Assert.NotNull(provider.GetService<IParticipantDao>());
+            Assert.NotNull(provider.GetService<IUploadDao>());
+            Assert.NotNull(provider.GetService<IParticipantApi>());
+        }
+    }
+}
