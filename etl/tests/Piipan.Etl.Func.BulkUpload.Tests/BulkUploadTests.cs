45d1163107a2dd15aa08001af77e01b1891bf156
Tim Ballard <timothy.ballard@gsa.gov>
2021-09-22 11:33:42 -0500

    BulkUpload tests

--- etl/tests/Piipan.Etl.Func.BulkUpload.Tests/BulkUploadTests.cs
+++ etl/tests/Piipan.Etl.Func.BulkUpload.Tests/BulkUploadTests.cs
@@ -5,123 +5,130 @@ using System.IO;
 using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Piipan.Etl.Func.BulkUpload.Models;
+using Piipan.Etl.Func.BulkUpload.Parsers;
+using Piipan.Participants.Api;
+using Piipan.Participants.Api.Models;
 using Xunit;
 
 namespace Piipan.Etl.Func.BulkUpload.Tests
 {
     public class BulkUploadTests
     {
-        // static Stream BadBlob()
-        // {
-        //     var stream = new MemoryStream();
-        //     var writer = new StreamWriter(stream);
-        //     writer.WriteLine("foo");
-        //     writer.Flush();
-        //     stream.Position = 0;
+        private Stream ToStream(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.WriteLine(s);
+            writer.Flush();
+            stream.Position = 0;
 
-        //     return stream;
-        // }
+            return stream;
+        }
 
-        // static PiiRecord AllFields()
-        // {
-        //     return new PiiRecord
-        //     {
-        //         LdsHash = LDS_HASH,
-        //         CaseId = "CaseId",
-        //         ParticipantId = "ParticipantId",
-        //         BenefitsEndDate = new DateTime(1970, 1, 1),
-        //         RecentBenefitMonths = new List<DateTime>() {
-        //           new DateTime(2021, 5, 31),
-        //           new DateTime(2021, 4, 30),
-        //           new DateTime(2021, 3, 31)
-        //         },
-        //         ProtectLocation = true
-        //     };
-        // }
+        private EventGridEvent EventMock()
+        {
+            var e = Mock.Of<EventGridEvent>();
+            // Can't override Data in Setup, just use a real one
+            e.Data = new Object();
+            return e;
+        }
 
-        // static PiiRecord OnlyRequiredFields()
-        // {
-        //     return new PiiRecord
-        //     {
-        //         LdsHash = LDS_HASH,
-        //         CaseId = "CaseId",
-        //         ParticipantId = null,
-        //         BenefitsEndDate = null,
-        //         RecentBenefitMonths = new List<DateTime>()
-        //     };
-        // }
+        private void VerifyLogError(Mock<ILogger> logger, String expected)
+        {
+            logger.Verify(x => x.Log(
+                It.Is<LogLevel>(l => l == LogLevel.Error),
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString() == expected),
+                It.IsAny<Exception>(),
+                It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)
+            ));
+        }
 
-        // static EventGridEvent EventMock()
-        // {
-        //     var e = Mock.Of<EventGridEvent>();
-        //     // Can't override Data in Setup, just use a real one
-        //     e.Data = new Object();
-        //     return e;
-        // }
+        [Fact]
+        public async void Run_NullInputStream()
+        {
+            // Arrange
+            var participantApi = Mock.Of<IParticipantApi>();
+            var participantStreamParser = Mock.Of<IParticipantStreamParser>();
+            var logger = new Mock<ILogger>();
+            var function = new BulkUpload(participantApi, participantStreamParser);
 
-        // // Check that the expected message was logged as an error at least once
-        // static void VerifyLogError(Mock<ILogger> logger, String expected)
-        // {
-        //     logger.Verify(x => x.Log(
-        //         It.Is<LogLevel>(l => l == LogLevel.Error),
-        //         It.IsAny<EventId>(),
-        //         It.Is<It.IsAnyType>((v, t) => v.ToString() == expected),
-        //         It.IsAny<Exception>(),
-        //         It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)
-        //     ));
-        // }
+            // Act
+            await function.Run(EventMock(), null, logger.Object);
 
-        
+            // Assert
+            VerifyLogError(logger, "No input stream was provided");
+        }
 
-        
+        [Fact]
+        public async void Run_ParserThrows()
+        {
+            // Arrange
+            var participantApi = Mock.Of<IParticipantApi>();
+            var participantStreamParser = new Mock<IParticipantStreamParser>();
+            participantStreamParser
+                .Setup(m => m.Parse(It.IsAny<Stream>()))
+                .Throws(new Exception("the parser broke"));
 
-        
+            var logger = new Mock<ILogger>();
+            var function = new BulkUpload(participantApi, participantStreamParser.Object);
 
-        
+            // Act / Assert
+            await Assert.ThrowsAsync<Exception>(() => function.Run(EventMock(), ToStream("data"), logger.Object));
+            VerifyLogError(logger, "the parser broke");
+        }
 
-        // [Fact]
-        // public async void CountInserts()
-        // {
-        //     var logger = Mock.Of<ILogger>();
-        //     var factory = new Mock<DbProviderFactory>() { DefaultValue = DefaultValue.Mock };
-        //     var cmd = new Mock<DbCommand>() { DefaultValue = DefaultValue.Mock };
-        //     factory.Setup(f => f.CreateCommand()).Returns(cmd.Object);
+        [Fact]
+        public async void Run_ApiThrows()
+        {
+            // Arrange
+            var participantApi = new Mock<IParticipantApi>();
+            participantApi
+                .Setup(m => m.AddParticipants(It.IsAny<IEnumerable<IParticipant>>()))
+                .Throws(new Exception("the api broke"));
 
-        //     // Mocks foreign key used in participants table
-        //     cmd.Setup(c => c.ExecuteScalar()).Returns((Int64)1);
+            var participantStreamParser = Mock.Of<IParticipantStreamParser>();
 
-        //     // Mock can't test unique constraint on SSN
-        //     var records = new List<PiiRecord>() {
-        //         AllFields(),
-        //         OnlyRequiredFields(),
-        //     };
-        //     await BulkUpload.Load(records, factory.Object, logger);
+            var logger = new Mock<ILogger>();
+            var function = new BulkUpload(participantApi.Object, participantStreamParser);
 
-        //     // Row in uploads table + rows in participants table
-        //     cmd.Verify(f => f.ExecuteNonQuery(), Times.Exactly(1 + records.Count));
-        // }
+            // Act / Assert
+            await Assert.ThrowsAsync<Exception>(() => function.Run(EventMock(), ToStream("data"), logger.Object));
+            VerifyLogError(logger, "the api broke");
+        }
 
-        // [Fact]
-        // public async void NoInputStream()
-        // {
-        //     var gridEvent = EventMock();
-        //     var logger = new Mock<ILogger>();
+        [Fact]
+        public async void Run_ParsedInputPassedToApi()
+        {
+            // Arrange
+            var participants = new List<Participant>
+            {
+                new Participant
+                {
+                    LdsHash = Guid.NewGuid().ToString(),
+                    CaseId = Guid.NewGuid().ToString(),
+                    ParticipantId = Guid.NewGuid().ToString(),
+                    BenefitsEndDate = DateTime.UtcNow,
+                    RecentBenefitMonths = new List<DateTime>(),
+                    ProtectLocation = (new Random()).Next(2) == 1
+                }
+            };
 
-        //     Stream input = null;
-        //     await BulkUpload.Run(gridEvent, input, logger.Object);
-        //     VerifyLogError(logger, "No input stream was provided");
-        // }
+            var participantStreamParser = new Mock<IParticipantStreamParser>();
+            participantStreamParser
+                .Setup(m => m.Parse(It.IsAny<Stream>()))
+                .Returns(participants);
 
-        // [Fact]
-        // public async void BadInputStream()
-        // {
-        //     var gridEvent = EventMock();
-        //     var logger = new Mock<ILogger>();
+            var participantApi = new Mock<IParticipantApi>();
+            var logger = new Mock<ILogger>();
+            var function = new BulkUpload(participantApi.Object, participantStreamParser.Object);
 
-        //     await Assert.ThrowsAnyAsync<Exception>(async () =>
-        //     {
-        //         await BulkUpload.Run(gridEvent, BadBlob(), logger.Object);
-        //     });
-        // }
+            // Act
+            await function.Run(EventMock(), ToStream("data"), logger.Object);
+
+            // Assert
+            participantApi.Verify(m => m.AddParticipants(participants), Times.Once);
+        }
     }
 }
