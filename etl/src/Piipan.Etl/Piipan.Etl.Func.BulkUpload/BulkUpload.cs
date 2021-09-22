ef27b5e28919c81fe22f2a30eba9cd0d5ae9f335
Tim Ballard <timothy.ballard@gsa.gov>
2021-09-22 09:59:09 -0500

    rm deprecated database interaction code

--- etl/src/Piipan.Etl/Piipan.Etl.Func.BulkUpload/BulkUpload.cs
+++ etl/src/Piipan.Etl/Piipan.Etl.Func.BulkUpload/BulkUpload.cs
@@ -1,21 +1,14 @@
 // Default URL for triggering event grid function in the local environment.
 // http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
 using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Common;
-using System.Globalization;
 using System.IO;
 using System.Threading.Tasks;
-using CsvHelper;
-using CsvHelper.Configuration;
 using Microsoft.Azure.EventGrid.Models;
-using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
-using Npgsql;
-using Piipan.Shared.Helpers;
+using Piipan.Etl.Func.BulkUpload.Parsers;
+using Piipan.Participants.Api;
 
 namespace Piipan.Etl.Func.BulkUpload
 {
@@ -24,8 +17,19 @@ namespace Piipan.Etl.Func.BulkUpload
     /// bulk import CSV files via Storage Containers, Event Grid, and
     /// PostgreSQL.
     /// </summary>
-    public static class BulkUpload
+    public class BulkUpload
     {
+        private readonly IParticipantApi _participantApi;
+        private readonly IParticipantStreamParser _participantParser;
+
+        public BulkUpload(
+            IParticipantApi participantApi,
+            IParticipantStreamParser participantParser)
+        {
+            _participantApi = participantApi;
+            _participantParser = participantParser;
+        }
+
         /// <summary>
         /// Entry point for the state-specific Azure Function instance
         /// </summary>
@@ -37,7 +41,7 @@ namespace Piipan.Etl.Func.BulkUpload
         /// to the per-state storage account and write access to the per-state database.
         /// </remarks>
         [FunctionName("BulkUpload")]
-        public async static Task Run(
+        public async Task Run(
             [EventGridTrigger] EventGridEvent eventGridEvent,
             [Blob("{data.url}", FileAccess.Read, Connection = "BlobStorageConnectionString")] Stream input,
             ILogger log)
@@ -48,8 +52,8 @@ namespace Piipan.Etl.Func.BulkUpload
             {
                 if (input != null)
                 {
-                    var records = Read(input, log);
-                    await Load(records, NpgsqlFactory.Instance, log);
+                    var participants = _participantParser.Parse(input);
+                    await _participantApi.AddParticipants(participants);
                 }
                 else
                 {
@@ -63,116 +67,6 @@ namespace Piipan.Etl.Func.BulkUpload
                 log.LogError(ex.Message);
                 throw;
             }
-        }
-
-        internal static IEnumerable<PiiRecord> Read(Stream input, ILogger log)
-        {
-            var reader = new StreamReader(input);
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = true,
-                TrimOptions = TrimOptions.Trim
-            };
-            var csv = new CsvReader(reader, config);
-            csv.Context.RegisterClassMap<PiiRecordMap>();
-
-            // Yields records as it is iterated over
-            return csv.GetRecords<PiiRecord>();
-        }
-
-        internal async static Task<string> ConnectionString()
-        {
-            // Environment variables (and placeholder) established
-            // during initial function app provisioning in IaC
-            const string CloudName = "CloudName";
-            const string DatabaseConnectionString = "DatabaseConnectionString";
-            const string PasswordPlaceholder = "{password}";
-            const string GovernmentCloud = "AzureUSGovernment";
-
-            // Resource ids for open source software databases in the public and
-            // US government clouds. Set the desired active cloud, then see:
-            // `az cloud show --query endpoints.ossrdbmsResourceId`
-            const string CommercialId = "https://ossrdbms-aad.database.windows.net";
-            const string GovermentId = "https://ossrdbms-aad.database.usgovcloudapi.net";
-
-            var resourceId = CommercialId;
-            var cn = Environment.GetEnvironmentVariable(CloudName);
-            if (cn == GovernmentCloud) {
-                resourceId = GovermentId;
-            }
-
-            var builder = new NpgsqlConnectionStringBuilder(
-                Environment.GetEnvironmentVariable(DatabaseConnectionString));
-
-            if (builder.Password == PasswordPlaceholder)
-            {
-                var provider = new AzureServiceTokenProvider();
-                var token = await provider.GetAccessTokenAsync(resourceId);
-                builder.Password = token;
-            }
-
-            return builder.ConnectionString;
-        }
-
-        internal async static Task Load(IEnumerable<PiiRecord> records, DbProviderFactory factory, ILogger log)
-        {
-            var connString = await ConnectionString();
-
-            using (var conn = factory.CreateConnection())
-            {
-                conn.ConnectionString = connString;
-                conn.Open();
-
-                // Assumes we want to process in an all-or-nothing fashion;
-                // i.e., a processing error in one record spoils the whole batch
-                var tx = conn.BeginTransaction();
-
-                using (var cmd = factory.CreateCommand())
-                {
-                    cmd.Connection = conn;
-                    cmd.CommandText = "INSERT INTO uploads (created_at, publisher) VALUES(now(), current_user)";
-                    cmd.ExecuteNonQuery();
-                }
-
-                Int64 lastval = 0;
-                using (var cmd = factory.CreateCommand())
-                {
-                    cmd.Connection = conn;
-                    cmd.CommandText = "SELECT lastval()";
-                    lastval = (Int64)cmd.ExecuteScalar();
-                }
-
-                foreach (var record in records)
-                {
-                    using (var cmd = factory.CreateCommand())
-                    {
-                        cmd.Connection = conn;
-                        cmd.CommandText = "INSERT INTO participants (lds_hash, upload_id, case_id, participant_id, benefits_end_date, recent_benefit_months, protect_location) " +
-                            "VALUES (@lds_hash, @upload_id, @case_id, @participant_id, @benefits_end_date, @recent_benefit_months::date[], @protect_location)";
-
-                        AddWithValue(cmd, DbType.String, "lds_hash", record.LdsHash);
-                        AddWithValue(cmd, DbType.Int64, "upload_id", lastval);
-                        AddWithValue(cmd, DbType.String, "case_id", record.CaseId);
-                        AddWithValue(cmd, DbType.String, "participant_id", (object)record.ParticipantId ?? DBNull.Value);
-                        AddWithValue(cmd, DbType.DateTime, "benefits_end_date", (object)record.BenefitsEndDate ?? DBNull.Value);
-                        AddWithValue(cmd, DbType.Object, "recent_benefit_months", (object)DateFormatters.FormatDatesAsPgArray(record.RecentBenefitMonths));
-                        AddWithValue(cmd, DbType.Boolean, "protect_location", (object)record.ProtectLocation ?? DBNull.Value);
-
-                        cmd.ExecuteNonQuery();
-                    }
-                }
-                tx.Commit();
-                conn.Close();
-            }
-        }
-
-        static void AddWithValue(DbCommand cmd, DbType type, String name, object value)
-        {
-            var p = cmd.CreateParameter();
-            p.DbType = type;
-            p.ParameterName = name;
-            p.Value = value;
-            cmd.Parameters.Add(p);
         }
     }
 }
