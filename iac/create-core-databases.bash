782c4921beb10c07a9e638b4bc994bd214997bd6
Michael King <michael.r.king@gsa.gov>
2021-09-23 09:18:42 -0400

    Create match records database in IaC
    - Update `db_create_managed_role` to allow other resource groups when
      creating managed identities
    - Make DDL re-runnable by handling case where ENUMs already exist
    - Fix typo in schema

--- iac/create-core-databases.bash
+++ iac/create-core-databases.bash
@@ -9,6 +9,7 @@ set_constants () {
   SUPERUSER=$DB_ADMIN_NAME
 
   METRICS_DB_NAME=metrics
+  MATCH_RECORDS_DB_NAME=matches
 
   VAULT_NAME=$PREFIX-kv-core-$ENV
   PG_SECRET_NAME=core-pg-admin
@@ -63,24 +64,33 @@ main () {
 
   db_set_env "$RESOURCE_GROUP" "$DB_SERVER_NAME" "$DB_ADMIN_NAME" "$PG_SECRET"
 
-  # Create and configure metrics DB
+  echo "Creating $METRICS_DB_NAME database and applying DDL"
   db_init "$METRICS_DB_NAME" "$SUPERUSER"
-
-  echo "Create $METRICS_DB_NAME table"
   db_apply_ddl "$METRICS_DB_NAME" ../metrics/ddl/metrics.sql
+
+  echo "Creating $MATCH_RECORDS_DB_NAME database and applying DDL"
+  db_init "$MATCH_RECORDS_DB_NAME" "$SUPERUSER"
+  db_apply_ddl "$MATCH_RECORDS_DB_NAME" ../match/ddl/match-record.sql
 
   db_config_aad "$RESOURCE_GROUP" "$DB_SERVER_NAME" "$PG_AAD_ADMIN"
   db_use_aad "$DB_SERVER_NAME" "$PG_AAD_ADMIN"
 
-  echo "Configuring database access for $METRICS_API_APP_NAME"
-  db_create_managed_role "$METRICS_DB_NAME" "$METRICS_API_APP_NAME"
+  echo "Configuring $METRICS_DB_NAME access for $METRICS_API_APP_NAME"
+  db_create_managed_role "$METRICS_DB_NAME" "$METRICS_API_APP_NAME" "$RESOURCE_GROUP"
   db_config_managed_role "$METRICS_DB_NAME" "$METRICS_API_APP_NAME"
   db_grant_read "$METRICS_DB_NAME" "$METRICS_API_APP_NAME"
 
-  echo "Configuring database access for $METRICS_COLLECT_APP_NAME"
-  db_create_managed_role "$METRICS_DB_NAME" "$METRICS_COLLECT_APP_NAME"
+  echo "Configuring $METRICS_DB_NAME access for $METRICS_COLLECT_APP_NAME"
+  db_create_managed_role "$METRICS_DB_NAME" "$METRICS_COLLECT_APP_NAME" "$RESOURCE_GROUP"
   db_config_managed_role "$METRICS_DB_NAME" "$METRICS_COLLECT_APP_NAME"
   db_grant_readwrite "$METRICS_DB_NAME" "$METRICS_COLLECT_APP_NAME"
+
+  local orchestrator
+  orchestrator=$(get_resources "$ORCHESTRATOR_API_TAG" "$MATCH_RESOURCE_GROUP")
+  echo "Configuring $MATCH_RECORDS_DB_NAME access for $orchestrator"
+  db_create_managed_role "$MATCH_RECORDS_DB_NAME" "$orchestrator" "$MATCH_RESOURCE_GROUP"
+  db_config_managed_role "$MATCH_RECORDS_DB_NAME" "$orchestrator"
+  db_grant_readwrite "$MATCH_RECORDS_DB_NAME" "$orchestrator"
 
   db_leave_aad $PG_AAD_ADMIN
 
