63c71e805225c597c300e973c2dce5fdd5562f2e
Tim Ballard <timothy.ballard@gsa.gov>
2021-10-08 12:25:44 -0500

    AzureTokenProviderTests

--- shared/src/Piipan.Shared/Authentication/AzureTokenProvider.cs
+++ shared/src/Piipan.Shared/Authentication/AzureTokenProvider.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Piipan.Shared.Authentication
@@ -13,20 +12,16 @@ namespace Piipan.Shared.Authentication
     {
         private readonly TokenCredential _tokenCredential;
         private readonly AzureTokenProviderOptions<T> _options;
-        private readonly ILogger<AzureTokenProvider<T>> _logger;
 
         public AzureTokenProvider(TokenCredential tokenCredential,
-            IOptions<AzureTokenProviderOptions<T>> options,
-            ILogger<AzureTokenProvider<T>> logger)
+            IOptions<AzureTokenProviderOptions<T>> options)
         {
             _tokenCredential = tokenCredential;
             _options = options.Value;
-            _logger = logger;
         }
 
         public async Task<string> RetrieveAsync()
         {
-            _logger.LogInformation(_options.ResourceUri);
             var context = new TokenRequestContext(new[] { _options.ResourceUri });
             var token = await _tokenCredential.GetTokenAsync(context, default(CancellationToken));
 
--- shared/tests/Piipan.Shared.Tests/Authentication/AzureTokenProviderTests.cs
+++ shared/tests/Piipan.Shared.Tests/Authentication/AzureTokenProviderTests.cs
@@ -1,14 +1,48 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Piipan.Shared.Authentication;
 using Moq;
 using Xunit;
+using Azure.Core;
+using Microsoft.Extensions.Options;
+using System.Threading;
 
 namespace Piipan.Shared.Tests.Authentication
 {
     public class AzureTokenProviderTests
     {
-        
+        [Fact]
+        public async Task RetrieveAsync_UsesConfiguredResourceUri()
+        {
+            // Arrange
+            var tokenCredential = new Mock<TokenCredential>();
+            tokenCredential
+                .Setup(m => m.GetTokenAsync(It.IsAny<TokenRequestContext>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new AccessToken("testToken", DateTimeOffset.FromUnixTimeSeconds(60)));
+
+            var options = new Mock<IOptions<AzureTokenProviderOptions<AzureTokenProviderTests>>>();
+            options
+                .Setup(m => m.Value)
+                .Returns(new AzureTokenProviderOptions<AzureTokenProviderTests> 
+                { 
+                    ResourceUri = "testUri"
+                });
+
+            var provider = new AzureTokenProvider<AzureTokenProviderTests>(
+                tokenCredential.Object,
+                options.Object);
+
+            // Act
+            var token = await provider.RetrieveAsync();
+
+            // Assert
+            Assert.Equal("testToken", token);
+            tokenCredential
+                .Verify(m => m.GetTokenAsync(
+                    It.Is<TokenRequestContext>(c => c.Scopes.Contains("testUri")),
+                    It.IsAny<CancellationToken>()),
+                    Times.Once);
+        }
     }
 }
