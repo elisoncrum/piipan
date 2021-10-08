1531c445f733a9a0b9b70b7400a4ea80c168a45f
Tim Ballard <timothy.ballard@gsa.gov>
2021-10-08 12:01:00 -0500

    update transitive deps

---
+++ shared/tests/Piipan.Shared.Tests/Authentication/AzureTokenProviderTests.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace Piipan.Shared.Tests.Authentication
+{
+    public class AzureTokenProviderTests
+    {
+        
+    }
+}
