73444f8d8d6d5beec02ee7fe6c5b5d62d36ad6eb
Tim Ballard <timothy.ballard@gsa.gov>
2021-10-12 12:11:58 -0500

    AuthorizedJsonApiClientTests

---
+++ shared/tests/Piipan.Shared.Tests/Http/AuthorizedJsonApiClientTests.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Piipan.Shared.Authentication;
+using Piipan.Shared.Http;
+using Moq;
+using Moq.Protected;
+using Xunit;
+
+namespace Piipan.Shared.Tests.Http
+{
+    public class AuthorizedJsonApiClientTests
+    {
+        [Fact]
+        public async Task PostAsync_SendsExpectedMessage()
+        {
+            // Arrange
+            var httpResponse = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("response body") };
+
+            var expectedRequest = new HttpRequestMessage(HttpMethod.Post, "https://tts.test/path");
+            var httpMessageHandler = new Mock<HttpMessageHandler>();
+            httpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync", 
+                    ItExpr.Is<HttpRequestMessage>(m => m.Method == HttpMethod.Post && m.RequestUri.ToString() == "https://tts.test/path"), 
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(httpResponse);
+
+            var client = new HttpClient(httpMessageHandler.Object)
+            {
+                BaseAddress = new Uri("https://tts.test")
+            };
+
+            var clientFactory = new Mock<IHttpClientFactory>();
+            clientFactory
+                .Setup(m => m.CreateClient(typeof(AuthorizedJsonApiClientTests).Name))
+                .Returns(client);
+            
+            var tokenProvider = new Mock<ITokenProvider<AuthorizedJsonApiClientTests>>();
+            var apiClient = new AuthorizedJsonApiClient<AuthorizedJsonApiClientTests>(
+                clientFactory.Object,
+                tokenProvider.Object
+            );
+            var body = new StringContent("this is the message body");
+            
+            // Act
+            var response = await apiClient.PostAsync("/path", body);
+
+            // Assert
+            var responseBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("response body", responseBody);
+        }
+    }
+}
