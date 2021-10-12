b5b7992133040b232b2b6607e2658190b4cd8c7c
Tim Ballard <timothy.ballard@gsa.gov>
2021-10-12 12:19:18 -0500

    basic test for GetAsync

--- shared/tests/Piipan.Shared.Tests/Http/AuthorizedJsonApiClientTests.cs
+++ shared/tests/Piipan.Shared.Tests/Http/AuthorizedJsonApiClientTests.cs
@@ -53,5 +53,45 @@ namespace Piipan.Shared.Tests.Http
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             Assert.Equal("response body", responseBody);
         }
+
+        [Fact]
+        public async Task GetAsync_SendsExpectedMessage()
+        {
+            // Arrange
+            var httpResponse = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("response body") };
+
+            var expectedRequest = new HttpRequestMessage(HttpMethod.Get, "https://tts.test/path");
+            var httpMessageHandler = new Mock<HttpMessageHandler>();
+            httpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync", 
+                    ItExpr.Is<HttpRequestMessage>(m => m.Method == HttpMethod.Get && m.RequestUri.ToString() == "https://tts.test/path"), 
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(httpResponse);
+
+            var client = new HttpClient(httpMessageHandler.Object)
+            {
+                BaseAddress = new Uri("https://tts.test")
+            };
+
+            var clientFactory = new Mock<IHttpClientFactory>();
+            clientFactory
+                .Setup(m => m.CreateClient(typeof(AuthorizedJsonApiClientTests).Name))
+                .Returns(client);
+            
+            var tokenProvider = new Mock<ITokenProvider<AuthorizedJsonApiClientTests>>();
+            var apiClient = new AuthorizedJsonApiClient<AuthorizedJsonApiClientTests>(
+                clientFactory.Object,
+                tokenProvider.Object
+            );
+            
+            // Act
+            var response = await apiClient.GetAsync("/path");
+
+            // Assert
+            var responseBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("response body", responseBody);
+        }
     }
 }
