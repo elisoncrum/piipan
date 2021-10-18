bddfaf0fc1e620c9afe29cd5d80cb71db81981c3
Tim Ballard <timothy.ballard@gsa.gov>
2021-10-18 15:10:24 -0500

    add annotations for MatchClient

--- match/src/Piipan.Match/Piipan.Match.Client/MatchClient.cs
+++ match/src/Piipan.Match/Piipan.Match.Client/MatchClient.cs
@@ -6,15 +6,29 @@ using Piipan.Shared.Http;
 
 namespace Piipan.Match.Client
 {
+    /// <summary>
+    /// HTTP client for interacting with Piipan.Match.Func.Api
+    /// </summary>
     public class MatchClient : IMatchApi
     {
         private readonly IAuthorizedApiClient<MatchClient> _apiClient;
 
+        /// <summary>
+        /// Intializes a new instance of MatchClient
+        /// </summary>
+        /// <param name="apiClient">an API client instance scoped to MatchClient</param>
         public MatchClient(IAuthorizedApiClient<MatchClient> apiClient)
         {
             _apiClient = apiClient;
         }
 
+        /// <summary>
+        /// Sends a POST request to the /find_matches endpoint using the API client's configured base URL.
+        /// Includes in the request headers the identifier of the state initiating the request
+        /// </summary>
+        /// <param name="request">A collection of participants to attempt to find matches for</param>
+        /// <param name="initiatingState">The two character identifier of the state initiating the request</param>
+        /// <returns></returns>
         public async Task<OrchMatchResponse> FindMatches(OrchMatchRequest request, string initiatingState)
         {
             return await _apiClient
