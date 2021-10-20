dd78794e40fa294e28a4c3210e8efeb475ab38d6
Michael King <michael.r.king@gsa.gov>
2021-10-20 16:28:17 -0400

    Remove unused IParticipantMatch interface

--- match/src/Piipan.Match/Piipan.Match.Api/Models/IParticipantMatch.cs
+++
@@ -1,9 +0,0 @@
-using Piipan.Participants.Api.Models;
-
-namespace Piipan.Match.Api.Models
-{
-    public interface IParticipantMatch : IParticipant
-    {
-        string MatchId { get; set; }
-    }
-}
