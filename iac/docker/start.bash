486c77c0ecd39d80b9a0b0ddda41f33fe902cb22
EdwinTorresHernandez <edwintorreshernandez@fcoh2j-g19ylvdf.lan>
2021-11-03 11:54:34 -0400

    Added start bash for docker, modify dockerfile and docker-compose

---
+++ iac/docker/start.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Script used by docker to start the provisioning of resources 
+# as part of the infraestruture as code process
+#
+azure_env=$1
+az login
+cd /piipan/iac
+echo "Starting create-resources.bash on ${azure_env} enviroment"
+./create-resources.bash $azure_env
